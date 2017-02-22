9d39f548bad80536fc8a423d1d864bc25d06b386
Suvineet Singh <suvineet.kalsi@student.manchester.ac.uk>
2017-02-22 22:05:23 +0000

    trying a new way og uploading. PHP script

--- upload.php
+++ upload.php
@@ -1,34 +1,56 @@
 <?php
-if(isset($_FILES['file'])) {
-  $file = $_FILES['file'];
-
-
-  //File properties
-  $file_name = $file['name'];
-  $file_tmp = $file['tmp_name'];
-  $file_size = $file['size'];
-  $file_error = $file['error'];
-
-  //Work out the file extensions
-  $file_ext = explode('.', $file_name);
-  $file_ext = strtolower(end($file_ext));
-
-  $allowed = array('jpg');
-
-  if (in_array($file_ext, $allowed))
-  {
-    if ($file_error === 0)
-    {
-      if ($file_size <= 2097152)
-      {
-        $file_name_new = uniqid('', true) . '.' . $file_ext;
-        $file_destination = '/var/www/html/NoteBoat/uploads/' . $file_name_new;
-
-        if (move_uploaded_file($file_temp, $file_destination))
-        {
-
-        }
-      }
-    }
-  }
+$file_result = "";
+if ($_FILES["file"]["error"] > 0)
+{
+  $file_result .= "No file uploaded or invalid file";
+  $file_result .= "Error Code: " .$_FILES["file"]["error"] . "<br>";
 }
+else
+{
+  $file_result . =
+  "Upload: " . $_FILES["file"]["name"] . "<br>" .
+  "Type: " . $_FILES["file"]["type"] . "<br>" .
+  "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br>" .
+  "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+  move_uploaded_file($_FILES["file"]["tmp_name"],
+  "/var/www/html/NOTEBOAT/uploads/" . $_FILES["file"]["name"]);
+
+  $file_result .= "File uploaded successfully";
+}
+//
+// if(isset($_FILES['file']))
+// {
+//   $file = $_FILES['file'];
+//
+//
+//   //File properties
+//   $file_name = $file['name'];
+//   $file_tmp = $file['tmp_name'];
+//   $file_size = $file['size'];
+//   $file_error = $file['error'];
+//
+//   //Work out the file extensions
+//   $file_ext = explode('.', $file_name);
+//   $file_ext = strtolower(end($file_ext));
+//
+//   $allowed = array('txt');
+//
+//   if (in_array($file_ext, $allowed))
+//   {
+//     if ($file_error === 0)
+//     {
+//       if ($file_size <= 2097152)
+//       {
+//         $file_name_new = uniqid('', true) . '.' . $file_ext;
+//         $file_destination = '/var/www/html/NoteBoat/uploads/' . $file_name_new;
+//
+//         if (move_uploaded_file($file_temp, $file_destination))
+//         {
+//           echo $file_destination;
+//         }
+//       }
+//     }
+//   }
+// }
+?>
