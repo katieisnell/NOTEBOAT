a92cea64320896b0c0eb54f783bad757533f8dea
Suvineet Singh Kalsi <suvineet.kalsi@student.manchester.ac.uk>
2017-03-04 19:44:17 +0000

    sql config file updated (had the wrong dbname in it before). Also upload.php edited so now file is uploaded and also stored in the sql

--- NotesSharing/upload.php
+++ NotesSharing/upload.php
@@ -3,60 +3,52 @@ $file_result = "";
 $loggedInUser = "mbaxask3";
 $fileModule = "MATH10111";
 $isFilePublic = 1;
+$newFileID = rand();
+$currentDateTime = date('d/m/Y h:i:s', time());
+
 if ($_FILES["file"]["error"] > 0)
 {
   $file_result .= "No file uploaded or invalid file";
   $file_result .= "Error Code: " .$_FILES["file"]["error"] . "<br>";
-}
+  echo $_FILES["file"]["size"];
+}//if
 else
 {
+  echo "FILE NAME: " . $_FILES["file"]["name"];
   if (file_exists("uploads/$loggedInUser/" . $_FILES["file"]["name"]))
   {
     $file_result .= "A file with the same name exists. Please choose a new name";
-  }
-  else
-   {
-     if ($_FILES["file"]["type"] === 'application/pdf')
-     {
-       move_uploaded_file($_FILES["file"]["tmp_name"],
-       "uploads/$loggedInUser/" . $_FILES["file"]["name"]);
-       
-       $file_result .= "File uploaded successfully";
-
-
- $newFileID = rand();
-  require_once('config.inc.php');
-  $conn = new mysqli($database_host, $database_user, $database_pass,
-                        $database_name);
-
-  if($conn -> connect_error)
+  }//if
+  else if ($_FILES["file"]["type"] === 'application/pdf')
   {
-    die('Connect Error ('.$conn -> connect_errno.')'.$conn -> connect_error);
+    move_uploaded_file($_FILES["file"]["tmp_name"],
+    "uploads/$loggedInUser/" . $_FILES["file"]["name"]);
+
+    $file_result .= "File uploaded successfully";
+
+    require_once('config.inc.php');
+    $conn = new mysqli($database_host, $database_user, $database_pass,
+                       $database_name);
+
+    if($conn -> connect_error)
+    {
+      die('Connect Error ('.$conn -> connect_errno.')'.$conn -> connect_error);
+    }
+
+    $fileName = $_FILES["file"]["name"] . "<" . $newFileID;
+    $query = "INSERT INTO `Notes` (`fileID`, `userID`, `fileName`, `fileModuleCode`, `uploadDate`, `filePublic`) VALUES ('$newFileID','$loggedInUser','$fileName','$fileModule', '$currentDateTime' , '$isFilePublic')";
+
+    $added = $conn -> query($query);
+      echo "Added to the DB YO!";
+      echo "<br>";
+      echo $query;
+      echo "<br>";
+      echo "added= " . $added;
+  }//if
+  else
+  {
+    $file_result .= "Only pdf files can be uploaded";
   }
-
-  $fileName = $_FILES["file"]["name"] . "<" . $fileID;
-  $query = "INSERT INTO Notes (`fileID`, `userID`, `fileName`, "
-           . "`fileModuleCode`, `uploadDate`, `filePublic`) VALUES "
-           . " ('$newFileID','$loggedInUser','$fileName','$fileModule', "
-           . "'" . date("d-m-Y h:i:sa") . "' , '$isFilePublic')";
-
-  $conn -> query($query);
-  echo "Added to the DB YO!";
-
-     }
-     else
-     {
-       $file_result .= "Only pdf files can be uploaded";
-     }
-  }
-
 }
 echo $file_result;
-
-function addFileToSQL()
-{
-echo "It ran add SQL";
- 
-  
-}
 ?>
