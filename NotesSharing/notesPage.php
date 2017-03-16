6c1a45631a53b549793d79c94b98f918aa193ce5
Suvineet Singh Kalsi <suvineet.kalsi@student.manchester.ac.uk>
2017-03-16 14:27:55 +0000

    Table generates for the files found. Need to figure a way to pass in parameters to the JS func from php

--- NotesSharing/notesPage.php
+++ NotesSharing/notesPage.php
@@ -1,68 +1,10 @@
-<?php
-
-$chosenModule = $_POST['modules'];
-
-require("config.inc.php");
-echo Sup!;
-$conn = new mysqli($database_host, $database_user, $database_pass, $database_name);
-
-if($conn -> connect_error)
-{
-	die('Connect Error ('.$conn -> connect_errno.')'.$conn -> connect_error);
-}
-
-
-$query = "SELECT * FROM Notes WHERE fileModuleCode = '$chosenModule'";
-
-$foundFiles = $conn -> query($query);
-$numOfFiles = mysqli_num_rows($foundFiles);
-
-<table id="myTable">
-  <tr>
-    <td>Row1 cell1</td>
-    <td>Row1 cell2</td>
-  </tr>
-  <tr>
-    <td>Row2 cell1</td>
-    <td>Row2 cell2</td>
-  </tr>
-  <tr>
-    <td>Row3 cell1</td>
-    <td>Row3 cell2</td>
-  </tr>
-</table>
-<br>
-
-<button onclick="myCreateFunction()">Create row</button>
-<button onclick="myDeleteFunction()">Delete row</button>
-
-<script>
-function myCreateFunction() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-}
-
-function myDeleteFunction() {
-    document.getElementById("myTable").deleteRow(0);
-}
-</script>
-for ($row = 1; $row <= numOfFiles; $row++)
-{
-
-}
-echo $numOfFiles;
-
-?>
 <html>
 <head>
   <title>Notes View</title>
 </head>
 
 <body>
+
 <h1>View notes</h1>
 <div>
 <form id="viewNotes" action="" method="post"></br>
@@ -83,6 +25,72 @@ echo $numOfFiles;
     <button type="submit" value="Refine" id="button"></button>
 </form>
 </div>
+<style>
+table, td {
+    border: 1px solid black;
+}
+</style>
+<div>
+	<table id="filesTable">
+		 <tr>
+  			  <td>Row1 cell1</td>
+    		  <td>Row1 cell2</td>
+  		</tr>
+	</table>
+	
+	<br>
+</div>
 
+<script>
+function createRow(name, module) 
+{
+    var table = document.getElementById("filesTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = name;
+    cell2.innerHTML = module;
+}
+
+function deleteRow() 
+{
+    document.getElementById("myTable").deleteRow(0);
+}
+</script>
 </body>
 </html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+$chosenModule = $_POST['module'];
+
+require("/home/pi/NOTEBOAT/config.inc.php");
+$conn = new mysqli($database_host, $database_user, $database_pass, $database_name);
+
+if($conn -> connect_error)
+{
+	die('Connect Error ('.$conn -> connect_errno.')'.$conn -> connect_error);
+}
+
+
+$query = "SELECT * FROM Notes WHERE fileModuleCode = '$chosenModule'";
+
+$foundFiles = $conn -> query($query);
+$numOfFiles = mysqli_num_rows($foundFiles);
+
+
+//for ($i = 1; $row <= $numOfFiles; $i++)
+//{
+	while($row = $foundFiles->fetch_assoc())
+	{
+		echo $row["fileName"];
+		$test =  '<script type="text/javascript"> createRow($row["fileName"] , $row["fileModuleCode"]); </script>';
+    }
+//}
+
+}
+?>
+
