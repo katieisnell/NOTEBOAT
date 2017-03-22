7af4c5ec702e67daf331d41b4c17694dd26ae8f6
Katie Snell <katie.snell@student.manchester.ac.uk>
2017-03-22 15:37:51 +0000

    trying to fix delete js11

--- BackEndTestCode/SQLCode/timetable.php
+++ BackEndTestCode/SQLCode/timetable.php
@@ -366,7 +366,7 @@ var m=0;
     var typeString=getType(type);
     $('#box').attr('id', 'box'+boxCount);
     $('#box'+boxCount ).html('<span> '+name+'<br>'+typeString+'</span>');
-    $('#box'+boxCount ).attr('oncontextmenu', alert("yo mama"););
+    $('#box'+boxCount ).attr('oncontextmenu', alert("yo mama"));
 
     document.getElementById('box'+boxCount).style.top=(((gridHeight/12) *dayY +1 )+'px');
     document.getElementById('box'+boxCount).style.left=((gridWidth*dayX +1)+'px');
