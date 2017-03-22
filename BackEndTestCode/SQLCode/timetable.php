bd025795bdaee6815a92615f68bd957c7f7f5c51
Katie Snell <katie.snell@student.manchester.ac.uk>
2017-03-22 15:34:09 +0000

    trying to fix delete js8

--- BackEndTestCode/SQLCode/timetable.php
+++ BackEndTestCode/SQLCode/timetable.php
@@ -357,15 +357,14 @@ var m=0;
     dayY = startTime % 288;
     var suvi =10;
     boxCount++
-    var remove = 'removeDiv($(this),'+type+','+name+','+startTime+','+duration+','+colour+');';
+    var remove = "'removeDiv($(this),'+type+','+name+','+startTime+','+duration+','+colour+');'";
     alert(remove);
 
 
-    $("<div class='box' id='box' style='left:0px; top:0px; min-height: 0px; background-color:yellow;'></div>").appendTo('#timetableMeat');
+    $("<div class='box' id='box' oncontextmenu=remove style='left:0px; top:0px; min-height: 0px; background-color:yellow;'></div>").appendTo('#timetableMeat');
     //
     var typeString=getType(type);
     $('#box').attr('id', 'box'+boxCount);
-    $('#box'+boxCount).attr('oncontextmenu', remove);
     $('#box'+boxCount ).html('<span> '+name+'<br>'+typeString+'</span>');
 
     document.getElementById('box'+boxCount).style.top=(((gridHeight/12) *dayY +1 )+'px');
