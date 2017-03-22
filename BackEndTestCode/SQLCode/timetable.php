13c3369bcf99a8877822ed74442dc6deffc931e3
Katie Snell <katie.snell@student.manchester.ac.uk>
2017-03-22 15:37:13 +0000

    trying to fix delete js10

--- BackEndTestCode/SQLCode/timetable.php
+++ BackEndTestCode/SQLCode/timetable.php
@@ -358,7 +358,7 @@ var m=0;
     var suvi =10;
     boxCount++
     var remove = 'removeDiv($(this),'+type+','+name+','+startTime+','+duration+','+colour+');';
-    alert(remove);
+    //alert(remove);
 
 
     $("<div class='box' id='box' oncontextmenu=''+remove style='left:0px; top:0px; min-height: 0px; background-color:yellow;'></div>").appendTo('#timetableMeat');
@@ -366,6 +366,7 @@ var m=0;
     var typeString=getType(type);
     $('#box').attr('id', 'box'+boxCount);
     $('#box'+boxCount ).html('<span> '+name+'<br>'+typeString+'</span>');
+    $('#box'+boxCount ).attr('oncontextmenu', alert("yo mama"););
 
     document.getElementById('box'+boxCount).style.top=(((gridHeight/12) *dayY +1 )+'px');
     document.getElementById('box'+boxCount).style.left=((gridWidth*dayX +1)+'px');
